1b3f25df1c682e734e9fe5855f5fbc5092bb5ab7
Ken Vernaillen <k.vernaillen@sig.eu>
2022-08-18 18:44:36 +0200

    exit fullscreen

--- pibooth_google_photo.py
+++ pibooth_google_photo.py
@@ -114,7 +114,9 @@ class GooglePhotosApi(object):
 
     def _auth(self):
         """Open browser to create credentials."""
-        self._window.toggle_fullscreen() # TODO
+        # TODO TEST
+        if self._window.is_fullscreen:
+            self._window.toggle_fullscreen()
         flow = InstalledAppFlow.from_client_secrets_file(self.client_id_file, scopes=self.SCOPES)
         return flow.run_local_server(port=0)
 
